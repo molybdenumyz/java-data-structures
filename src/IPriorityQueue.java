ffa4602ea4e3f816791ef750225d3c8141a217a2
尹哲 <yinzhe@hrsoft.net>
2017-10-19 22:04:44 +0800

    增加poll方法

--- src/IPriorityQueue.java
+++ src/IPriorityQueue.java
@@ -1,4 +1,5 @@
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Created by yinzhe on 17/10/18.
@@ -18,37 +19,40 @@ public class IPriorityQueue<E> {
     public IPriorityQueue(int initialCapacity,
                           Comparator<? super E> comparator) {
 
-        if (initialCapacity < 1)
+        if (initialCapacity < 1) {
             throw new IllegalArgumentException();
+        }
 
         this.queue = new Object[initialCapacity];
 
         this.comparator = comparator;
     }
+
     public IPriorityQueue(Comparator<? super E> comparator) {
         this(DEFAULT_INITIAL_CAPACITY, comparator);
     }
 
-    public boolean offer(E insert){
-        if (insert == null){
+    public boolean offer(E insert) {
+        if (insert == null) {
             throw new NullPointerException();
         }
         int i = size;
 
-        if (i>=queue.length){
+        if (i >= queue.length) {
 
         }
 
-        if (i == 0)
+        if (i == 0) {
             queue[0] = insert;
-        else
-            siftUp(i,insert);
+        } else {
+            siftUp(i, insert);
+        }
 
         size = size + 1;
         return true;
     }
 
-    public void siftUp(int position,E insert){
+    public void siftUp(int position, E insert) {
 
 
         for (;(comparator.compare(insert,(E) queue[position>>>1]) < 0)&&(position > 0);position = position>>>1){
@@ -58,7 +62,7 @@ public class IPriorityQueue<E> {
         queue[position] = insert;
     }
 
-    public boolean add(E e){
+    public boolean add(E e) {
         return offer(e);
     }
 
@@ -68,13 +72,55 @@ public class IPriorityQueue<E> {
 
     private int indexOf(Object o) {
         if (o != null) {
-            for (int i = 0; i < size; i++)
-                if (o.equals(queue[i]))
+            for (int i = 0; i < size; i++) {
+                if (o.equals(queue[i])) {
                     return i;
+                }
+            }
         }
         return -1;
     }
 
 
+    public E poll() {
 
+        if(size == 0) {
+            return null;
+        }
+
+        E output = (E) queue[0];
+
+        E s = (E) queue[--size];
+
+        queue[size] = null;
+
+        if (size != 0){
+            siftDown(0,  s);
+        }
+        return output;
+    }
+
+    public void siftDown(int position,E key) {
+
+        int child;
+
+        int half = size >>> 1;
+
+        for (;position < half;position = child){
+            child = (position << 1) +1;
+
+
+            if (child+1 < size && comparator.compare((E)queue[child],(E) queue[child+1]) > 0 ){
+                child = child + 1 ;
+            }
+
+            if (comparator.compare(key,(E) queue[child]) <= 0) {
+                break;
+            }
+
+            queue[position] = queue[child];
+        }
+
+        queue[position] = key;
+    }
 }
